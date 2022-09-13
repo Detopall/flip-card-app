b432c90a4bf1ae8754080e80818960dddcf8f2dc
denis.topallaj <denis.topallaj@student.howest.be>
2022-09-13 10:42:55 +0200

    finished the turning of cards, works, will remove db and add dummy info

--- website/static/script.js
+++ website/static/script.js
@@ -3,5 +3,41 @@
 document.addEventListener("DOMContentLoaded", init);
 
 function init(){
-	
+	document.addEventListener("click", turnCard);
+}
+
+function turnCard(e){
+	if (!e.target.closest("article")) return;
+	const article = e.target.closest("article");
+	if (article.classList.contains("turn-to-back")){
+		showBack(article);
+	} else {
+		showFront(article);
+	}
+}
+
+function showBack(article){
+	const frontText = article.querySelector("p").innerHTML;
+	const backText = article.getAttribute("data-back");
+	let html = `
+	<h2> ${frontText} </h2>
+	<p> ${backText} </p>
+	`;
+	article.innerHTML = "";
+	article.insertAdjacentHTML("beforeend", html);
+	article.classList.remove("turn-to-back");
+	article.classList.add("turn-to-front");
+}
+
+function showFront(article){
+	const frontText = article.querySelector("h2").innerHTML;
+	const descriptionText = article.getAttribute("data-description");
+	let html = `
+	<h2> ${descriptionText} </h2>
+	<p> ${frontText} </p>
+	`;
+	article.innerHTML = "";
+	article.insertAdjacentHTML("beforeend", html);
+	article.classList.add("turn-to-back");
+	article.classList.remove("turn-to-front");
 }
